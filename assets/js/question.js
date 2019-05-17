5e8498615e41bceb35fbfdb32a8841d2941a64a8
setiawanfarism <farissetiawan@gmail.com>
2019-05-17 14:52:41 +0700

    Preject-Quiz

---
+++ assets/js/question.js
@@ -0,0 +1,37 @@
+var questions = [
+    {
+        "kuis": "Ibukota Negara Indonesia ?",
+        "option1": "Surabaya",
+        "option2": "Bandung",
+        "option3": "Jakarta",
+        "option4": "Medan",
+        "answer": "3"
+    }, {
+        "kuis": "Nama dari prosesor Intel generasi 5 ?",
+        "option1": "Broadwell",
+        "option2": "Skylake",
+        "option3": "Haswell",
+        "option4": "Ivy Bridge",
+        "answer": "1"
+    }, {
+        "kuis": "Nama Julukan bagi Kota Semarang ?",
+        "option1": "Kota Serambi Mekah",
+        "option2": "Kota Metropolitan",
+        "option3": "Paris van Java",
+        "option4": "Kota Atlas",
+        "answer": "4"
+    }, {
+        "kuis": "Linux yang menggunakan Package Management DEB,kecuali ?",
+        "option1": "Arch Linux",
+        "option2": "Ubuntu",
+        "option3": "Solus",
+        "option4": "PCLinux Os",
+        "answer": "2"
+    }, {
+        "kuis": "Bahasa Pemrograman yang termasuk OOP ?",
+        "option1": "C#",
+        "option2": "C",
+        "option3": "Pascal",
+        "option4": "FORTRAN",
+        "answer": "4"
+    }];
